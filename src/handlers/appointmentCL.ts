86943d8d6b931821207ed99e0288cd243cf0884b
Victor <vatorresm88@gmail.com>
2025-09-11 13:34:03 -0500

    feat: Mejorar el manejo de eventos SQS en el controlador de citas para procesar correctamente los mensajes de SNS

--- src/handlers/appointmentCL.ts
+++ src/handlers/appointmentCL.ts
@@ -4,8 +4,18 @@ import { AppointmentService } from '../services/appointmentService';
 const service = new AppointmentService();
 
 export const handler = async (event: SQSEvent) => {
-  for (const record of event.Records) {
-    const appointment = JSON.parse(record.body);
-    await service.processCountry(appointment, 'PE');
+  try {
+    for (const record of event.Records) {
+      console.log('Mensaje SQS recibido:', record.body);
+      const snsMessage = JSON.parse(record.body);
+      console.log('Mensaje SNS parseado:', snsMessage);
+      const appointment = JSON.parse(snsMessage.Message);
+      console.log('Appointment parseado:', JSON.stringify(appointment));
+      await service.processCountry(appointment, 'CL');
+      console.log('Cita procesada correctamente');
+    }
+  } catch (error) {
+    console.error('Error en el procesamiento:', error);
+    throw error;
   }
 };
